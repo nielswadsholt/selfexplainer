5a4fffc0ec34a2ac928e8b7ce4d16c95d7908dd1
Niels Wadsholt <nwadsholt@ymail.com>
2020-05-24 21:48:34 +0200

    Add shiny new button

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 	<a class="github-button" href="https://github.com/nielswadsholt/selfexplainer" data-color-scheme="no-preference: dark; light: light; dark: dark;" data-size="large" aria-label="View nielswadsholt/selfexplainer on GitHub">View on GitHub</a>
 	<h1>{name}</h1>
 	<p>Today a basic <a href="https://svelte.dev/tutorial" target="blank">Svelte tutorial</a> rip-off. Tomorrow the world (hello, by the way).</p>
-	<p></p>
+	<button>Button</button>
 </main>
 
 <style>
@@ -18,8 +18,18 @@
 		margin: 0 auto;
 	}
 
+	button {
+		background-color: #801515;
+		border: none;
+		border-radius: 0.4em;
+		color: antiquewhite;
+		cursor: pointer;
+		font-size: 2em;
+		transition-duration: 0.5s;
+	}
+
 	h1 {
-		color: #ff3e00;
+		color: #801515;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
